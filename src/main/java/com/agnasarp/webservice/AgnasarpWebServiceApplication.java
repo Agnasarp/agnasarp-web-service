5e716c8c7dfd59bd084ced4ac9cc7c34638d88a8
Hasitha_08884 <hasitha.abewardana@dialog.lk>
2021-07-15 00:13:31 +0530

    Initial commit

---
+++ src/main/java/com/agnasarp/webservice/AgnasarpWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agnasarp.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgnasarpWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgnasarpWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agnasarp/webservice/AgnasarpWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnasarp.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgnasarpWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
